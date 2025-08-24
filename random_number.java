9ade7e7abee8240bcc80d5554b3a68ba732ee21e
SURYA-Btech <sec23cb051@sairamtap.edu.in>
2025-08-24 15:58:10 +0530

    Add files via upload

---
+++ random_number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class jc
+{
+public static void main(String[] dfs)
+
+{ 
+byte d, f=0;
+int ran;
+System.out.print("This is a random number guessing game");
+Random r = new Random();
+ran=r.nextInt(1,2);
+System.out.println(" "+ran);
+Scanner s = new Scanner (System.in);
+while(f==0)
+{
+System.out.print("Enter the random number between 1 to 4:");
+d=s.nextByte();
+if(d==ran)
+{System.out.println("You won");
+f=1;}
+
+else {f=0;}
+}
+}
+}
+
+
